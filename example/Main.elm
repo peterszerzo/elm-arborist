96edb1df62f84255fa2f6bc2652212a129ae0925
peterszerzo <szerzo.peter@gmail.com>
2017-08-31 15:23:03 +0200

    Factor out styles

--- example/Main.elm
+++ example/Main.elm
@@ -7,7 +7,7 @@ import Html.Events exposing (onInput)
 import Tree
 import Item
 import Treditor
-import Colors exposing (..)
+import Styles
 
 
 type alias Model =
@@ -33,31 +33,17 @@ update msg model =
             { model | treditor = Treditor.update treditorConfig treditorMsg model.treditor }
 
 
-boxStyle : List ( String, String )
-boxStyle =
-    [ ( "width", "560px" )
-    , ( "height", "560px" )
-    , ( "margin", "10px" )
-    , ( "text-align", "left" )
-    , ( "display", "inline-block" )
-    , ( "vertical-align", "top" )
-    , ( "position", "relative" )
-    , ( "border-radius", "4px" )
-    , ( "border", "1px solid " ++ faintGray )
-    ]
-
-
 view : Model -> Html Msg
 view model =
     div []
-        [ node "style" [] [ text rawStyle ]
+        [ node "style" [] [ text Styles.raw ]
         , div [ class "intro" ]
             [ h1 [] [ text "treeditor" ]
             , p [] [ text "Prototype for an interface that manipulates a tree structure. Swap nodes by dragging them on top of each other." ]
             ]
         , div [ style [ ( "text-align", "center" ) ] ]
-            [ Treditor.view treditorConfig [ style boxStyle ] model.treditor |> Html.map TreditorMsg
-            , div [ style <| boxStyle ++ [ ( "padding", "20px" ) ] ]
+            [ Treditor.view treditorConfig [ style Styles.box ] model.treditor |> Html.map TreditorMsg
+            , div [ style <| Styles.box ++ [ ( "padding", "20px" ) ] ]
                 [ Treditor.active treditorConfig model.treditor
                     |> Maybe.map
                         (\item ->
@@ -69,60 +55,6 @@ view model =
                 ]
             ]
         ]
-
-
-rawStyle : String
-rawStyle =
-    """
-* {
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-  font-family: monospace;
-}
-
-.intro {
-  text-align: center;
-}
-
-input {
-  outline: none;
-  display: block;
-  width: 100%;
-  padding: 4px 8px;
-  margin-top: 4px;
-  border-radius: 4px;
-  border: 1px solid """ ++ faintGray ++ """;
-  transition: border-color 0.3s;
-}
-
-h1 {
-  font-size: 2rem;
-  font-weight: 400;
-}
-
-h3 {
-  font-size: 1.5rem;
-  font-weight: 400;
-}
-
-h3:not(:first-child) {
-  margin-top: 40px;
-}
-
-input:focus {
-  border-color: """ ++ blue ++ """;
-}
-
-label {
-  color: """ ++ gray ++ """;
-}
-
-input,
-label,
-p {
-  font-size: 1rem;
-}
-"""
 
 
 example : Tree.Tree Item.Item
---
+++ example/Styles.elm
@@ -0,0 +1,71 @@
+module Styles exposing (..)
+
+import Colors exposing (..)
+
+
+raw : String
+raw =
+    """
+* {
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  font-family: monospace;
+}
+
+.intro {
+  text-align: center;
+}
+
+input {
+  outline: none;
+  display: block;
+  width: 100%;
+  padding: 4px 8px;
+  margin-top: 4px;
+  border-radius: 4px;
+  border: 1px solid """ ++ faintGray ++ """;
+  transition: border-color 0.3s;
+}
+
+h1 {
+  font-size: 2rem;
+  font-weight: 400;
+}
+
+h3 {
+  font-size: 1.5rem;
+  font-weight: 400;
+}
+
+h3:not(:first-child) {
+  margin-top: 40px;
+}
+
+input:focus {
+  border-color: """ ++ blue ++ """;
+}
+
+label {
+  color: """ ++ gray ++ """;
+}
+
+input,
+label,
+p {
+  font-size: 1rem;
+}
+"""
+
+
+box : List ( String, String )
+box =
+    [ ( "width", "560px" )
+    , ( "height", "560px" )
+    , ( "margin", "10px" )
+    , ( "text-align", "left" )
+    , ( "display", "inline-block" )
+    , ( "vertical-align", "top" )
+    , ( "position", "relative" )
+    , ( "border-radius", "4px" )
+    , ( "border", "1px solid " ++ faintGray )
+    ]
