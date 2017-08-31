ff4652625d1ca33e7fb18cb83cb3ce63b26779d2
peterszerzo <szerzo.peter@gmail.com>
2017-08-31 15:53:52 +0200

    Fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,5 @@
 rm -rf build
 mkdir build
-elm-make example/Main.elm --output=build/index.html
-surge build elm-treditor.peterszerzo.com
+cd example
+elm-make Main.elm --output=../build/index.html
+surge ../build elm-treditor.peterszerzo.com
