a85c81be4517ece4377d39a18bf30e347d320e34
peterszerzo <szerzo.peter@gmail.com>
2017-10-06 13:14:57 +0200

    Add index.html to example

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,6 @@
 rm -rf build
 mkdir build
 cd example
-elm-make Main.elm --output=../build/index.html
+elm-make Main.elm --output=../build/elm.js
+cp index.html ../build
 surge ../build elm-arborist.peterszerzo.com
