7b46182b20118eaccbae07265f35414c038d9613
peterszerzo <szerzo.peter@gmail.com>
2017-12-29 10:25:30 +0100

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,6 +7,7 @@ cp example/index.html build
 git branch -D gh-pages
 git checkout --orphan gh-pages
 cp build/* .
+trash src example tests
 git add .
 git commit -m "Deploy"
 git push -f origin gh-pages
